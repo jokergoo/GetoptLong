a4bb548f90c36afeedec3091d7e75fef5a40b162
Zuguang Gu <jokergoo@gmail.com>
2013-10-28 19:05:24 +0100

    Update test.R

--- test/test.R
+++ test/test.R
@@ -1,6 +1,4 @@
-source("../R/qq.R")
-source("../R/GetoptLong.R")
-
+library(GetoptLong)
 
 options("GetoptLong.startingMsg" = "
 Usage:
@@ -18,146 +16,119 @@ VERSION = "0.0.1"
 spec = c(
 	"tag=i", "this is a description of tag which is long long and very long and extremly long..."
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 1 --tag 2")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
-GetoptLong(spec, argv_str = "")
+GetoptLong(spec, argv_str = "--tag 1");         expect_that(tag, equals(1))
+GetoptLong(spec, argv_str = "--tag 1 --tag 2"); expect_that(tag, equals(2))
+expect_that(GetoptLong(spec, argv_str = "--tag 0.1"), throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag a"),   throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag"),     throws_error())
+expect_that(GetoptLong(spec, argv_str = ""),          throws_error())
 
 spec = c(
 	"tag=s", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
+GetoptLong(spec, argv_str = "--tag 1");   expect_that(tag, is_identical_to("1"))
+GetoptLong(spec, argv_str = "--tag 0.1"); expect_that(tag, is_identical_to("0.1"))
+GetoptLong(spec, argv_str = "--tag a");   expect_that(tag, is_identical_to("a"))
+GetoptLong(spec, argv_str = "--tag");     expect_that(tag, is_identical_to(""))
 
 spec = c(
 	"tag=f", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
+GetoptLong(spec, argv_str = "--tag 1");   expect_that(tag, equals(1))
+GetoptLong(spec, argv_str = "--tag 0.1"); expect_that(tag, equals(0.1))
+expect_that(GetoptLong(spec, argv_str = "--tag a"),  throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag"),  throws_error())
 
 spec = c(
 	"tag=o", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 0b001001")
-GetoptLong(spec, argv_str = "--tag 0721")
-GetoptLong(spec, argv_str = "--tag 0xaf2")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
-
-spec = c(
-	"tag:i", "desc"
-)
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
-GetoptLong(spec, argv_str = "")
-
-
-spec = c(
-	"tag:s", "desc"
-)
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
-
-spec = c(
-	"tag:f", "desc"
-)
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
-
+GetoptLong(spec, argv_str = "--tag 1");        expect_that(tag, equals(1))
+GetoptLong(spec, argv_str = "--tag 0b001001"); expect_that(tag, equals(1))
+GetoptLong(spec, argv_str = "--tag 0721");     expect_that(tag, equals(1))
+GetoptLong(spec, argv_str = "--tag 0xaf2");    expect_that(tag, equals(1))
+expect_that(GetoptLong(spec, argv_str = "--tag 0.1"), throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag a"),   throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag"),     throws_error())
 
 spec = c(
 	"tag", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 0")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
-GetoptLong(spec, argv_str = "")
+GetoptLong(spec, argv_str = "--tag 1");   expect_that(tag, is_identical_to(TRUE))
+GetoptLong(spec, argv_str = "--tag 0");   expect_that(tag, is_identical_to(TRUE))
+GetoptLong(spec, argv_str = "--tag 0.1"); expect_that(tag, is_identical_to(TRUE))
+GetoptLong(spec, argv_str = "--tag a");   expect_that(tag, is_identical_to(TRUE))
+GetoptLong(spec, argv_str = "--tag");     expect_that(tag, is_identical_to(TRUE))
+GetoptLong(spec, argv_str = "");          expect_that(tag, is_identical_to(FALSE))
 
 spec = c(
 	"tag=i@", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 1 --tag 2")
-GetoptLong(spec, argv_str = "--tag 1 --tag a")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
-GetoptLong(spec, argv_str = "")
+GetoptLong(spec, argv_str = "--tag 1");         expect_that(tag, is_identical_to(1))
+GetoptLong(spec, argv_str = "--tag 1 --tag 2"); expect_that(tag, is_identical_to(1:2))
+expect_that(GetoptLong(spec, argv_str = "--tag 1 --tag a"), throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag 0.1"),       throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag a"),         throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag"),           throws_error())
+expect_that(GetoptLong(spec, argv_str = ""),                throws_error())
 
 spec = c(
 	"tag=i%", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag name=1")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
-GetoptLong(spec, argv_str = "")
+GetoptLong(spec, argv_str = "--tag name=1"); expect_that(tag, is_identical_to(c(name = 1)))
+expect_that(GetoptLong(spec, argv_str = "--tag 1"),           throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag 0.1"),         throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag a"),           throws_error())
+expect_that(expect_that(GetoptLong(spec, argv_str = "--tag"), throws_error())
+expect_that(GetoptLong(spec, argv_str = ""),                  throws_error())
 
 
 spec = c(
 	"tag=i{2}", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 1 2")
-GetoptLong(spec, argv_str = "--tag 0.1")
-GetoptLong(spec, argv_str = "--tag a")
-GetoptLong(spec, argv_str = "--tag")
-GetoptLong(spec, argv_str = "")
+GetoptLong(spec, argv_str = "--tag 1 2"); expect_that(tag, is_identical_to(1:2))
+expect_that(GetoptLong(spec, argv_str = "--tag 1"),   throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag 0.1"), throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag a"),   throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag"),     throws_error())
+expect_that(GetoptLong(spec, argv_str = ""),          throws_error())
 
 spec = c(
 	"tag=i{2,3}", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 1 2")
-GetoptLong(spec, argv_str = "--tag 1 2 3")
-GetoptLong(spec, argv_str = "--tag 1 2 3 4")
+GetoptLong(spec, argv_str = "--tag 1 2");   expect_that(tag, is_identical_to(1:2))
+GetoptLong(spec, argv_str = "--tag 1 2 3"); expect_that(tag, is_identical_to(1:3))
+expect_that(GetoptLong(spec, argv_str = "--tag 1"),       throws_error())
+expect_that(GetoptLong(spec, argv_str = "--tag 1 2 3 4"), throws_error())
 
 spec = c(
 	"tag=i{2,}", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 1 2")
-GetoptLong(spec, argv_str = "--tag 1 2 3")
-GetoptLong(spec, argv_str = "--tag 1 2 3 4")
+expect_that(GetoptLong(spec, argv_str = "--tag 1"), throws_error())
+GetoptLong(spec, argv_str = "--tag 1 2");     expect_that(tag, is_identical_to(1:2))
+GetoptLong(spec, argv_str = "--tag 1 2 3");   expect_that(tag, is_identical_to(1:3))
+GetoptLong(spec, argv_str = "--tag 1 2 3 4"); expect_that(tag, is_identical_to(1:4))
 
 spec = c(
 	"tag=i{,3}", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 1 2")
-GetoptLong(spec, argv_str = "--tag 1 2 3")
-GetoptLong(spec, argv_str = "--tag 1 2 3 4")
+GetoptLong(spec, argv_str = "--tag 1");     expect_that(tag, is_identical_to(1))
+GetoptLong(spec, argv_str = "--tag 1 2");   expect_that(tag, is_identical_to(1:2))
+GetoptLong(spec, argv_str = "--tag 1 2 3"); expect_that(tag, is_identical_to(1:3))
+expect_that(GetoptLong(spec, argv_str = "--tag 1 2 3 4"), throws_error())
 
 spec = c(
 	"tag=i{,}", "desc"
 )
-GetoptLong(spec, argv_str = "--tag 1")
-GetoptLong(spec, argv_str = "--tag 1 2")
-GetoptLong(spec, argv_str = "--tag 1 2 3")
-GetoptLong(spec, argv_str = "--tag 1 2 3 4")
+GetoptLong(spec, argv_str = "--tag 1");       expect_that(tag, is_identical_to(1))
+GetoptLong(spec, argv_str = "--tag 1 2");     expect_that(tag, is_identical_to(1:2))
+GetoptLong(spec, argv_str = "--tag 1 2 3");   expect_that(tag, is_identical_to(1:3))
+GetoptLong(spec, argv_str = "--tag 1 2 3 4"); expect_that(tag, is_identical_to(1:4))
 
 spec = c(
 	"verbose!", "print messages"
 )
-GetoptLong(spec, argv_str = "--verbose")
-GetoptLong(spec, argv_str = "--no-verbose")
+GetoptLong(spec, argv_str = "--verbose");    expect_that(tag, is_identical_to(TRUE))
+GetoptLong(spec, argv_str = "--no-verbose"); expect_that(tag, is_identical_to(FALSE))
 
 options("GetoptLong.Config" = "bundling")
 spec = c(
@@ -166,3 +137,24 @@ spec = c(
 	"yellow|y", "using yellow"
 )
 GetoptLong(spec, argv_str = "--red --blue --yellow")
+expect_that(red, is_identical_to(TRUE))
+expect_that(blue, is_identical_to(TRUE))
+expect_that(yellow, is_identical_to(TRUE))
+
+# with default value
+spec = c(
+	"tag=i", "this is a description of tag which is long long and very long and extremly long..."
+)
+tag = 4
+GetoptLong(spec, argv_str = "--tag 1");  expect_that(tag, equals(1))
+GetoptLong(spec, argv_str = "");         expect_that(tag, equals(4))
+
+# message
+expect_that(GetoptLong(spec, argc_str = "--version"), print_text())
+rm(VERSION)
+expect_that(GetoptLong(spec, argc_str = "--version"), print_text())
+expect_that(GetoptLong(spec, argc_str = "--version", version = FALSE), throws_error())
+
+expect_that(GetoptLong(spec, argc_str = "--help"), print_text())
+expect_that(GetoptLong(spec, argc_str = "--help", version = FALSE), throws_error())
+
