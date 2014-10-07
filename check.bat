f35f7536ff93c60c1782ac4334853a45a33e9f9c
jokergoo <jokergoo@gmail.com>
2014-10-07 20:37:24 +0200

    changes with GlobalOptions

--- check.bat
+++
@@ -1,7 +0,0 @@
-perl -I../circlize/lib -MR::Comment2Man -e "R::Comment2Man->draft('R')"
-
-cd ..
-R CMD REMOVE GetoptLong
-R CMD build --compact-vignettes=gs+qpdf GetoptLong
-R CMD check --as-cran --timings GetoptLong_0.0.7.tar.gz
-R CMD INSTALL GetoptLong_0.0.7.tar.gz
